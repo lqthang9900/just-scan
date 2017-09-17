eb9cb97d5dbaa6d5cc6655cd20546207c9ac7460
Le Thang <lqthang9900@gmail.com>
2017-09-17 12:38:48 +0700

    git clear

--- platforms/android/build/intermediates/assets/F0F1/debug/app/pages/sign-up/sign-up.ts
+++
@@ -1,8 +0,0 @@
-import { EventData } from 'data/observable';
-import { Page } from 'ui/page';
-import { SignUpViewModel } from './sign-up.model';
-
-export function navigatingTo(args: EventData) {
-	let page = <Page>args.object;
-	page.bindingContext = new SignUpViewModel();
-}
